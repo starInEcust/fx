b80b84799a31ddd4fe98400b980400510f6fc25e
starInEcust <fx154492313@hotmail.com>
2014-05-09 13:09:39 +0800

    增加样式

--- nodejs/app/js/directive.js
+++ nodejs/app/js/directive.js
@@ -3,149 +3,157 @@
  */
 
 app.directive('selectTable', ['dateData', '$rootScope', function (dateData, $rootScope) {
-    return{
-        restrict: "AE",
-        replace: true,
-        template: '<div class="element">' +
-            '<div class="input-control text" id="datepicker">' +
-            '<input type="text">' +
-            '<button class="btn-date"></button>' +
-            '</div>' +
-            '</div>',
-        link: function (scope, elem, attrs) {
-            var objdate = new Date();
+	return{
+		restrict: "AE",
+		replace: true,
+		template: '<div class="element">' +
+			'<div class="input-control text" id="datepicker">' +
+			'<input type="text">' +
+			'<button class="btn-date"></button>' +
+			'</div>' +
+			'</div>',
+		link: function (scope, elem, attrs) {
+			var objdate = new Date();
 
 
-            elem.children('#datepicker').datepicker({
-                date: objdate.getFullYear() + '-' + '0' + (objdate.getMonth() + 1) + '-' + (objdate.getDate()-1), // set init date
-                effect: "slide", // none, slide, fade
-                position: "bottom",
-                locale: 'en',
-                selected: function () {
-                    if(oldDate == getDate()){return}
-                    oldDate = getDate();
-                    dateData.getData(getDate()).then(function (data) {
-                        dateData.data = data;
+			elem.children('#datepicker').datepicker({
+				date: objdate.getFullYear() + '-' + '0' + (objdate.getMonth() + 1) + '-' + (objdate.getDate() - 1), // set init date
+				effect: "slide", // none, slide, fade
+				position: "bottom",
+				locale: 'en',
+				selected: function () {
+					if (oldDate == getDate()) {
+						return
+					}
+					oldDate = getDate();
+					dateData.getData(getDate()).then(function (data) {
+						dateData.data = data;
 //                        console.log(dateData.data);
-                        $rootScope.$broadcast('date.update');
-                    });
+						$rootScope.$broadcast('date.update');
+					});
 //                console.log(dateData.data)
-                }
-            });
-            function getDate() {
-                var date = elem.find('#datepicker input').val();
-                date = date.split('.').reverse().join('');
-                $rootScope.date = date;
-                return date;
-            }
-            var oldDate = getDate();
+				}
+			});
+			function getDate() {
+				var date = elem.find('#datepicker input').val();
+				date = date.split('.').reverse().join('');
+				$rootScope.date = date;
+				return date;
+			}
+
+			var oldDate = getDate();
 
 
-        }
-    }
+		}
+	}
 }]);
 app.directive('makeChart', function () {
-    return {
-        restrict: 'E',
-        template: '<div style="width:100%; height:400px;"></div>',
-        replace: 'true',
-        link: function (scope, elem, attrs) {
-            elem.highcharts({
-                chart: {
-                    type: 'bar'
-                },
-                title: {
-                    text: 'Fruit Consumption'
-                },
-                xAxis: {
-                    categories: ['Apples', 'Bananas', 'Oranges']
-                },
-                yAxis: {
-                    title: {
-                        text: 'Fruit eaten'
-                    }
-                },
-                series: [
-                    {
-                        name: 'Jane',
-                        data: [1, 0, 4]
-                    },
-                    {
-                        name: 'John',
-                        data: [5, 7, 3]
-                    }
-                ]
-            });
-        }
+	return {
+		restrict: 'E',
+		template: '<div style="width:100%; height:400px;"></div>',
+		replace: 'true',
+		link: function (scope, elem, attrs) {
+			elem.highcharts({
+				chart: {
+					type: 'bar'
+				},
+				title: {
+					text: 'Fruit Consumption'
+				},
+				xAxis: {
+					categories: ['Apples', 'Bananas', 'Oranges']
+				},
+				yAxis: {
+					title: {
+						text: 'Fruit eaten'
+					}
+				},
+				series: [
+					{
+						name: 'Jane',
+						data: [1, 0, 4]
+					},
+					{
+						name: 'John',
+						data: [5, 7, 3]
+					}
+				]
+			});
+		}
 
-    }
+	}
 });
 app.directive('activeLi', function () {
-    return {
-        restrict: 'AE',
-        link: function (scope, elem, attrs) {
-            elem.not('.title').on('click', function () {
-                elem.addClass('active').siblings().removeClass('active');
-            });
-        }
-    };
-});
-app.directive('typeSelect',['$rootScope','dateData',function($rootScope,dateData){
-    return {
-        restrict: 'AE',
-        link: function (scope, elem, attrs) {
-            var flag = elem.text();
-            elem.on('click',function(){
-                $rootScope.chartType = flag;
-                elem.parent().siblings().find('span').text(flag);
-                dateData.getData($rootScope.date).then(function (data) {
-                    dateData.data = data;
-//                        console.log(dateData.data);
-                    $rootScope.$broadcast('date.update');
-                });
-            });
-
-
-        }
-    };
-}]);
-app.directive('tdSwitch', function () {
 	return {
 		restrict: 'AE',
-			link: function (scope, elem, attrs) {
-				elem.on('click', function () {
-					scope.$apply(function(){
-						scope[attrs.tdSwitch]  = !scope[attrs.tdSwitch] ;
-					});
+		link: function (scope, elem, attrs) {
+			elem.not('.title').on('click', function () {
+				elem.addClass('active').siblings().removeClass('active');
 			});
 		}
 	};
 });
-app.directive('toggleAlluser', function () {
+app.directive('typeSelect', ['$rootScope', 'dateData', function ($rootScope, dateData) {
 	return {
 		restrict: 'AE',
 		link: function (scope, elem, attrs) {
+			var flag = elem.text();
 			elem.on('click', function () {
-				scope.$apply(function(){
-//					console.log(attrs.colspan);
-					if(attrs.toggleAlluser == 'All'){
+				$rootScope.chartType = flag;
+				elem.parent().siblings().find('span').text(flag);
+				dateData.getData($rootScope.date).then(function (data) {
+					dateData.data = data;
+//                        console.log(dateData.data);
+					$rootScope.$broadcast('date.update');
+				});
+			});
+
+
+		}
+	};
+}]);
+app.directive('tdSwitch', function () {
+	return {
+		restrict: 'AE',
+//		template: '<li ng-class="{buttonLight:isLight}" ng-transclude></li>',
+		replace: true,
+//		transclude: true,
+		link: function (scope, elem, attrs) {
+			scope.isLight = false;
+			elem.on('click', function () {
+				elem.toggleClass('buttonLight');
+				scope.$apply(function () {
+					if (attrs.tdSwitch) {
+						scope[attrs.tdSwitch] = !scope[attrs.tdSwitch];
+					}
+					if (attrs.toggleAlluser == 'All') {
 						console.log(attrs.toggleAlluser);
 						scope.all = false;
 						scope.user = true;
 						scope.spanNum = 2;
-					}else if(attrs.toggleAlluser == 'USER'){
+					} else if (attrs.toggleAlluser == 'USER') {
 						console.log(attrs.toggleAlluser);
 						scope.all = true;
 						scope.user = false;
 						scope.spanNum = 2;
-					}else{
+					} else {
 						console.log(attrs.toggleAlluser);
 						scope.all = false;
 						scope.user = false;
 						scope.spanNum = 1;
 					}
-//					scope[attrs.alluser]  = !scope[attrs.alluser] ;
 				});
+			});
+		}
+	};
+});
+
+app.directive('trSelect', function () {
+	return {
+		restrict: 'AE',
+		link: function (scope, elem, attrs) {
+			elem.on('click', function () {
+				elem.toggleClass('info');
 			});
 		}
 	};
