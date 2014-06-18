7de1c9360091868a122eedd15b5b95389330ad4a
starInEcust <fx154492313@hotmail.com>
2014-06-18 10:35:24 +0800

    ADD TYPE 460

--- nodejs/models/getdatedata.js
+++ nodejs/models/getdatedata.js
@@ -4,25 +4,33 @@
 var fs = require('fs');
 var fullData = {};
 module.exports = {
-    'dateData': function (req, response) {
-        var date = req.body.date;
-        var regexFlag = req.body.flag;
-        console.log(regexFlag);
-        fs.readFile('app/data/noah_push_statistic_' + date, {'encoding': 'utf-8'}, function (err, data) {
-            var regex = '/^'+regexFlag+'/i';
-            regex = eval(regex);
-            console.log(typeof (regex));
-            var a = eval("(" + data + ")");
-            for (var key in a) {
-                if (!regex.test(key)) {
-                    delete a[key];
-                }
-            }
-            var json = JSON.stringify(a);
-            console.log(json);
-            response.send(json);
-        });
-    }
+	'dateData': function (req, response) {
+		var date = req.body.date;
+		var regexFlag = req.body.flag;
+		console.log(regexFlag);
+		fs.readFile('app/data/noah_push_statistic_' + date, {'encoding': 'utf-8'}, function (err, data) {
+			var regex = '/^' + regexFlag + '/i';
+			regex = eval(regex);
+			console.log(typeof (regex));
+			var a = eval("(" + data + ")");
+			if (regexFlag == 'Dialer_OEM') {
+				for (var key in a) {
+					if (!regex.test(key) && key != 'OEM002025002' && key != 'OEM042035001') {
+						delete a[key];
+					}
+				}
+			} else {
+				for (var key in a) {
+					if (!regex.test(key)) {
+						delete a[key];
+					}
+				}
+			}
+			var json = JSON.stringify(a);
+			console.log(json);
+			response.send(json);
+		});
+	}
 //    'timeBucket':function(request,response){
 ////
 //        var dateStart = request.body.dateStart;
